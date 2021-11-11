a274eaeb4400d134de4d9f61a5a309d3df9861f4
Amoss Dvir <amoss.dvir@verint.com>
2021-11-11 16:55:11 +0200

    Finished

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,26 @@
+import "./SeasonDisplay.css";
 import react from "react";
 
-const SeasonDisplay = () => {
-    return <div>Season</div>
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date());
+  const { text, iconName } =
+    season === "winter"
+      ? { text: "Brr... It's Chilly", iconName: "snowflake" }
+      : { text: "Let Go To The Beach!", iconName: "sun" };
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import react from "react";
-import ReactDOM  from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 
-const App = () => {
+class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { lat: null, errMessage: "" };
+//   }
+
+// Constructor method declaration is equivalent to this line: 
+state = { lat: null, errMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-        );
-    return <div className="ui content">Test</div>
-};
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMessage: err.message })
+    );
+  }
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+  render() {
+    // Show Error message:
+    if (this.state.errMessage && !this.state.lat) {
+      return (
+        <div className="ui content">
+          There was an error: {this.state.errMessage}
+        </div>
+      );
+    }
+    // Show Latitude:
+    if (!this.state.errMessage && this.state.lat) {
+      return <div className="ui content">
+      <SeasonDisplay lat={this.state.lat}></SeasonDisplay>
+      </div>;
+    }
+    return <div className="ui content"><Loader message="Please Allow Location"></Loader></div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
